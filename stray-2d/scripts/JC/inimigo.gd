f5514b5587d920b887a0cab70ccdfe97e57088c7
JoaoAVCunha <133639053+JoaoAVCunha@users.noreply.github.com>
2025-04-14 13:05:08 +0100

    Resoluçao de bug
    bug no inimigo

--- stray-2d/scripts/JC/inimigo.gd
+++ stray-2d/scripts/JC/inimigo.gd
@@ -8,6 +8,7 @@ var killed = 1
 @onready var detect_left: RayCast2D = $Detect_left
 @onready var detect_right: RayCast2D = $Detect_right
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player: CharacterBody2D = $"../Player"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -37,16 +38,22 @@ func _process(delta: float) -> void:
 func restartlevel():
 	get_tree().reload_current_scene()
 
+func killcat():
+	
+	get_node("Damage_hitbox/CollisionShape2D").queue_free()
+
+# Levar hit de inimigo
 func _on_damage_hitbox_body_entered(_body: Node2D) -> void:
 	print("Skill issue")
 	call_deferred("restartlevel")
 
-
+# Matar o inimigo
 func _on_kill_hitbox_body_entered(_body: Node2D) -> void:
 	killed = 0
 	animation.play("death")
-	get_node("Damage_hitbox/CollisionShape2D").free()
+	call_deferred("killcat")
 
-
+# Quando a animaçao de morto acabar remover nodo do inimigo
 func _on_animated_sprite_2d_animation_finished() -> void:
 	get_node(".").queue_free()
+	player.velocity.y += -100
