eef0705aa514d8692792960b950b5682b406520e
alex07guedes <alex07.guedes@gmail.com>
2025-05-16 15:44:15 +0100

    boss

--- stray-2d/scripts/pistao.gd
+++ stray-2d/scripts/pistao.gd
@@ -1,23 +1,21 @@
 extends Node2D
-
-@export var piston_left_path: NodePath
-@export var piston_right_path: NodePath
-@export var trigger_area_path: NodePath
-
-@onready var piston_left: NodePath = get_node_or_null($"../piston_left_path")
-@onready var piston_right: Node = get_node_or_null($"../piston_right")
-@onready var trigger_area: Area2D = get_node_or_null(trigger_area_path)
-
+@onready var timer: Timer = $Timer
+var piston_left
+var isDoor = false
 func _ready() -> void:
-	if piston_left and piston_right and trigger_area:
-		trigger_area.body_entered.connect(_on_area_2d_body_entered)
-
-func _on_area_2d_body_entered(body: Node) -> void:
-		var anim_left = piston_left.get_node_or_null("AnimationPlayer")
-		var anim_right = piston_right.get_node_or_null("AnimationPlayer")
+	piston_left = get_meta("piston_left")
+	print(piston_left)
+	if get_node_or_null(piston_left):
+		isDoor = true
+	timer.wait_time = get_meta("delay")
+	if not isDoor:
+		timer.start()
 		
-		if anim_left and anim_right:
-			anim_left.stop(true)
-			anim_right.stop(true)
-			anim_left.play("close")
-			anim_right.play("close")
+func  _process(delta: float) -> void:
+	if isDoor:
+		if get_node_or_null(piston_left) == null:
+			print("open")
+			$AnimationPlayer.play("idle")
+
+func _on_timer_timeout() -> void:
+	$AnimationPlayer.play("idle")
