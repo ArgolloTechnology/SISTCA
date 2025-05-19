09169547fac12139ee6460fd9743977f099f06b2
alex07guedes <alex07.guedes@gmail.com>
2025-05-19 09:20:39 +0100

    boss pistao

--- stray-2d/area_2d.gd
+++ stray-2d/area_2d.gd
@@ -3,6 +3,9 @@ extends Area2D
 @export var zoom_out: Vector2 = Vector2(2, 2)
 @export var zoom_time: float = 0.5
 
+@export var boss_path: NodePath  # arrasta aqui o Boss no Inspetor
+@onready var boss = get_node_or_null(boss_path)
+
 func _on_area_2d_body_entered(body: Node) -> void:
 	if body.has_node("Camera2D"):
 		var camera = body.get_node("Camera2D") as Camera2D
@@ -10,7 +13,13 @@ func _on_area_2d_body_entered(body: Node) -> void:
 		# Animação de zoom usando Tween (opcional, visualmente mais suave)
 		var tween := create_tween()
 		tween.tween_property(camera, "zoom", zoom_out, zoom_time)
-
+		
+	if body.name == "Player":
+		var boss = get_node_or_null(boss_path)
+		if boss:
+			boss.activate_enemy()
+		else:
+			print("❌ Boss não encontrado! Verifica o path.")
 
 func _on_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
--- stray-2d/scripts/pistao.gd
+++ stray-2d/scripts/pistao.gd
@@ -3,17 +3,17 @@ extends Node2D
 var piston_left
 var isDoor = false
 func _ready() -> void:
-	piston_left = get_meta("piston_left")
-	print(piston_left)
-	if get_node_or_null(piston_left):
-		isDoor = true
+	#piston_left = get_meta("piston_left")
+	#print(piston_left)
+	#if get_node_or_null(piston_left):
+	#	isDoor = true
 	timer.wait_time = get_meta("delay")
 	if not isDoor:
 		timer.start()
 		
 func  _process(delta: float) -> void:
 	if isDoor:
-		if get_node_or_null(piston_left) == null:
+		#if get_node_or_null(piston_left) == null:
 			print("open")
 			$AnimationPlayer.play("idle")
 
