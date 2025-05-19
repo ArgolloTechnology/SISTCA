6d6994c6089a48f062598e585bf9fe57ea4fa210
JoaoAVCunha <133639053+JoaoAVCunha@users.noreply.github.com>
2025-05-19 08:48:16 +0100

    Retoques
    Separar scripts dos finais
    Main menu path to main

--- stray-2d/scripts/JC/main_menu.gd
+++ stray-2d/scripts/JC/main_menu.gd
@@ -1,21 +1,13 @@
 extends Control
 
-
-
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/JC/nivel_inimigo.tscn")
-
-
+	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
 
-
-
 func _on_credits_pressed() -> void:
 	pass # Replace with function body.
-
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
